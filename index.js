9f1aca1f2fba0afe53b01a91170f42693689b018
George Joubran <durd2001@gmail.com>
2021-10-27 15:14:35 +0300

    Added some coloring, Added transition animations on hover

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const newsApiKey = 'e578049dacd34ecdb9206efe077eec92'
 const weatherInput = document.getElementById("city-input");
 const submitForm = document.getElementById("city-submit")
 const form = document.getElementById("news-form")
-
-let currCountry = "";
+const allArtTitles = Array.from(document.querySelectorAll(".art1Title"))
+const allArtContents = Array.from(document.querySelectorAll(".art1Content"))
+const allArtImages = Array.from(document.querySelectorAll(".art1Img"))
+const readMore = Array.from(document.querySelectorAll(".button"))
+const weatherCard = document.querySelector(".weather6")
+const weatherTitle = document.getElementById("weatherTitle")
+const weatherImg = document.getElementById("weatherImg")
+const windText = document.getElementById("windText")
+const humidityText = document.getElementById("humidityText")
+const weatherType = document.getElementById("weatherType")
+const weatherTemp = document.getElementById("weatherTemp")
+const date = document.querySelector('.currDate')
 
 fetchIt('Haifa');
 
@@ -18,50 +28,35 @@ form.addEventListener("submit", event => {
     }
 
 })
+
 function fetchIt(weatherInputValue) {
     fetch(`http://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${weatherInputValue}&days=7&aqi=no&alerts=no`).then(response => {
         if (!response.ok) throw new Error(response.status);
         return response.json();
     }).then(json1 => {
         console.log(json1);
-        currCountry = countryCodes.find(element => element.Name == json1.location.country).Code.toLowerCase();
+        let currCountry = countryCodes.find(element => element.Name == json1.location.country).Code.toLowerCase();
         fetch(`https://newsapi.org/v2/top-headlines?country=${currCountry}&apiKey=${newsApiKey}`).then(response => {
             if (!response.ok) throw new Error(response.status);
             return response.json();
         }).then(json => {
             console.log(json);
             const articlesArr = json.articles;
-            let index = 0;
-            const allArtTitles = Array.from(document.querySelectorAll(".art1Title"))
-            const allArtContents = Array.from(document.querySelectorAll(".art1Content"))
-            const allArtImages = Array.from(document.querySelectorAll(".art1Img"))
-            const readMore = Array.from(document.querySelectorAll(".button"))
-
             let fixedArticles = articlesArr.filter(item => item.urlToImage != null && item.description != '');
-
-
-            allArtTitles.forEach((element) => {
+            allArtTitles.forEach((element, index) => {
                 element.textContent = fixedArticles[index].title;
-                index++;
             })
-            index = 0;
-            allArtContents.forEach((element) => {
+            allArtContents.forEach((element, index) => {
                 element.textContent = fixedArticles[index].description;
-
-                index++;
             })
-            index = 0;
-            allArtImages.forEach((element) => {
+            allArtImages.forEach((element, index) => {
                 element.src = fixedArticles[index].urlToImage;
                 element.width = "80"
                 element.height = "30"
-                index++;
             })
-            index = 0;
-            readMore.forEach((element) => {
+            readMore.forEach((element, index) => {
                 element.href = fixedArticles[index].url;
                 element.style.display = 'block'
-                index++;
             })
         }).catch(error => {
             if (error.message === "400") {
@@ -71,14 +66,7 @@ function fetchIt(weatherInputValue) {
                 console.log(`Unexpected Error`);
             }
         });
-        const weatherCard = document.querySelector(".weather6")
-        const weatherTitle = document.getElementById("weatherTitle")
-        const weatherImg = document.getElementById("weatherImg")
-        const windText = document.getElementById("windText")
-        const humidityText = document.getElementById("humidityText")
-        const weatherType = document.getElementById("weatherType")
-        const weatherTemp = document.getElementById("weatherTemp")
-        const date = document.querySelector('.currDate')
+
         weatherTitle.textContent = json1.location.name + " - " + json1.location.country;
         weatherImg.src = json1.current.condition.icon
         windText.textContent = "Wind: " + json1.current.wind_kph + "Km/h"
@@ -86,9 +74,6 @@ function fetchIt(weatherInputValue) {
         weatherTemp.textContent = json1.current.temp_c + "\u2103"
         weatherType.textContent = json1.current.condition.text
         date.textContent = json1.forecast.forecastday[0].date
-        weatherCard.dir = 'ltr'
-
-
 
     }).catch(error => {
         if (error.message === "400") {
